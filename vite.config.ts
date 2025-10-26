a6945c523c9455c15facc3938870b2020908b5b7
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-10-26 16:10:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    tsconfigPaths(), // resolves @/ aliases
-  ],
-  base: mode === 'development' ? '/' : '/slick-cut-landing/',
-}));
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+});
